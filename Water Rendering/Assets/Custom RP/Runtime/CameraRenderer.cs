a727606ae8eb6d983a3acb72bb9ef3b0d219972c
DeckardZ46 <deckardz46@outlook.com>
2022-07-27 22:52:53 +0100

    DeckardZ | Set up project | 20220727

---
+++ Water Rendering/Assets/Custom RP/Runtime/CameraRenderer.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class CameraRenderer
+{
+    ScriptableRenderContext context;
+    Camera camera;
+    public void Render(ScriptableRenderContext context, Camera camera)
+    {
+        this.context = context;
+        this.camera = camera;
+
+        DrawVisibleGeometry();
+        Submit();
+    }
+
+    void Submit (){
+        context.Submit();
+    }
+    void DrawVisibleGeometry()
+    {
+        context.DrawSkybox(camera);
+    }
+
+}
+
